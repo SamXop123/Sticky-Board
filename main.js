1c393ba49e4f8e419aee84e371e6fa1bd91c1452
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-07-12 00:50:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ const path = require('path');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.sendFile('templates/index.html', { root: __dirname });
 });
+
+const tasks = [];
+app.post('/submit-task', (req, res) => {
+  const { title, description } = req.body;
+  const id = Date.now(); // Unique ID
+  tasks.push({ id, title, description });
+  console.log('New Task:', title, '\nDescription:', description);
+  res.json({ id, title, description });
+});
+
 
 app.listen(port, () => {
   console.log(`StickyBoard Running on port ${port}`);
